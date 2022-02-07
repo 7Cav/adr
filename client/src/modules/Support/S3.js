64f3daa008d696d1750bb8f12baecdcf58c5a8a6
Vercin-G <97210222+Vercin-G@users.noreply.github.com>
2022-02-06 22:07:08 -0700

    Development

---
+++ client/src/modules/Support/S3.js
@@ -0,0 +1,111 @@
+import React from 'react';
+
+function S3List (listArray) {
+    var {listArray} = listArray
+    var array = [];
+    var s3Positions = ['134','135','136','137','138','139','140','141','142','143','144','145','146','147','148','149','150','151','152','153','154','155','156','157','158','159','540','541','542','543'];
+
+    for (var milpacIdCombat in listArray[0].combat.profiles) {
+
+        var name = listArray[0].combat.profiles[milpacIdCombat].realName;
+        var rank = listArray[0].combat.profiles[milpacIdCombat].rank.rankFull 
+        var primary = listArray[0].combat.profiles[milpacIdCombat].primary;
+        var fullName = rank + ' ' + name;
+        var primarySortKey = listArray[0].combat.profiles[milpacIdCombat].primary.positionId;
+
+        if (s3Positions.includes(primary.positionId)) {
+
+            array.push({
+                "fullName": fullName,
+                "position": primary,
+                "isPrimary": 'true',
+                "sortKey": primarySortKey,
+                "itemKey": name
+                
+            })
+        }
+
+        for (var index in listArray[0].combat.profiles[milpacIdCombat].secondaries) {
+            var secondary = listArray[0].combat.profiles[milpacIdCombat].secondaries[index]
+            var secondarySortKey = listArray[0].combat.profiles[milpacIdCombat].secondaries[index].positionId;
+
+            if (!s3Positions.includes(secondary.positionId)) {
+                continue;
+            }
+
+            array.push({
+                "fullName": fullName,
+                "position": secondary,
+                "isPrimary": 'false',
+                "sortKey": secondarySortKey,
+                "itemKey": name
+            })
+        }
+    }; 
+
+    for (var milpacIdReserve in listArray[0].reserve.profiles) {
+
+        var rName = listArray[0].reserve.profiles[milpacIdReserve].realName;
+        var rRank = listArray[0].reserve.profiles[milpacIdReserve].rank.rankFull 
+        var rPrimary = listArray[0].reserve.profiles[milpacIdReserve].primary;
+        var rFullName = rRank + ' ' + rName;
+        var rPrimarySortKey = listArray[0].reserve.profiles[milpacIdReserve].primary.positionId;
+
+        if (s3Positions.includes(rPrimary.positionId)) {
+
+            array.push({
+                "fullName": rFullName,
+                "position": rPrimary,
+                "isPrimary": 'true',
+                "sortKey": rPrimarySortKey,
+                "itemKey": rName
+            })
+        }
+
+        for (var rIndex in listArray[0].reserve.profiles[milpacIdReserve].secondaries) {
+            var rSecondary = listArray[0].reserve.profiles[milpacIdReserve].secondaries[rIndex]
+            var rSecondarySortKey = listArray[0].reserve.profiles[milpacIdReserve].secondaries[rIndex].positionId;
+
+            if (!s3Positions.includes(rSecondary.positionId)) {
+                continue;
+            }
+
+            array.push({
+                "fullName": rFullName,
+                "position": rSecondary,
+                "isPrimary": 'false',
+                "sortKey": rSecondarySortKey,
+                "itemKey": rName
+            })
+        }
+    }; 
+
+    array.sort((a,b) => a.sortKey - b.sortKey)
+    
+    return(
+        <div className='WAGList'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Billet</th>
+                        <th>sortKey</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {array.map( obj => {
+                        return(
+                            <tr key={index}>
+                                <td>{obj.fullName}</td>
+                                <td>{obj.position.positionTitle}</td>
+                                <td>{obj.sortKey}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default S3List;
