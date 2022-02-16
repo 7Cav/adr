09d90ba7077e06a8c1229ef88022536781534e7c
Vercin-G <97210222+Vercin-G@users.noreply.github.com>
2022-02-16 16:16:10 -0700

    Minimum Viable Product

---
+++ client/src/modules/1-7/Bravo.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+function Bravo1List (listArray) {
+    var {listArray} = listArray
+    var array = [];
+    var bravoPositions = ['227','228','229','230','231','232','233','234','235','236','237','238','239','240','241','242','243','244','245','246','247','248','249','250','251','252','253','254','255','256','257','258','259','260','261','262','263','264','265','266','267','268','269','270','271','272','273','274','275','276','277','278','279','280','281','282','283','284','285'];
+
+    for (var milpacIdCombat in listArray[0].combat.profiles) {
+
+        var name = listArray[0].combat.profiles[milpacIdCombat].realName;
+        var rank = listArray[0].combat.profiles[milpacIdCombat].rank.rankFull 
+        var primary = listArray[0].combat.profiles[milpacIdCombat].primary;
+        var fullName = rank + ' ' + name;
+        var primarySortKey = listArray[0].combat.profiles[milpacIdCombat].primary.positionId;
+
+        if (bravoPositions.includes(primary.positionId)) {
+
+            array.push({
+                "fullName": fullName,
+                "position": primary,
+                "isPrimary": 'true',
+                "sortKey": primarySortKey,
+                "itemKey": name
+                
+            })
+        }
+    }; 
+
+    array.sort((a,b) => a.sortKey - b.sortKey)
+    
+    return(
+        <div className='WAGList'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Billet</th>
+                        <th>sortKey</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {array.map( obj => {
+                        return(
+                            <tr>
+                                <td>{obj.fullName}</td>
+                                <td>{obj.position.positionTitle}</td>
+                                <td>{obj.sortKey}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Bravo1List
---
+++ client/src/modules/2-7/Bravo.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+function Bravo2List (listArray) {
+    var {listArray} = listArray
+    var array = [];
+    var bravoPositions = ['390','391','392','393','394','395','396','397','398','399','400','401','402','403','404','405','406','407','408','409','410','411','412','413','414','415','416','417','418','419','420','421','422','423','424','425','426','427','428','429','430','431','432','433','434'];
+
+    for (var milpacIdCombat in listArray[0].combat.profiles) {
+
+        var name = listArray[0].combat.profiles[milpacIdCombat].realName;
+        var rank = listArray[0].combat.profiles[milpacIdCombat].rank.rankFull 
+        var primary = listArray[0].combat.profiles[milpacIdCombat].primary;
+        var fullName = rank + ' ' + name;
+        var primarySortKey = listArray[0].combat.profiles[milpacIdCombat].primary.positionId;
+
+        if (bravoPositions.includes(primary.positionId)) {
+
+            array.push({
+                "fullName": fullName,
+                "position": primary,
+                "isPrimary": 'true',
+                "sortKey": primarySortKey,
+                "itemKey": name
+                
+            })
+        }
+    }; 
+
+    array.sort((a,b) => a.sortKey - b.sortKey)
+    
+    return(
+        <div className='WAGList'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Billet</th>
+                        <th>sortKey</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {array.map( obj => {
+                        return(
+                            <tr>
+                                <td>{obj.fullName}</td>
+                                <td>{obj.position.positionTitle}</td>
+                                <td>{obj.sortKey}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Bravo2List
