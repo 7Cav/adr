14e54f3eabe184e844e501b80d82faa0afb93d6e
Vercin-G <97210222+Vercin-G@users.noreply.github.com>
2022-02-16 16:22:54 -0700

    Minimum Viable Product

--- server/credentials/token.js
+++
@@ -1,6 +0,0 @@
-Token = 'gDCIDy5ktY1Jkaj0OhtaDdWvoIQL0PJot4B1XjGT6edyuSVnf2'
-module.exports = Token;
-
-
-//rename this file as "token.js" when in use.
-// should be in `../server/credentials/token.js` path.
