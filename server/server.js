e1397ea742dff8d572a81dec17e5e8ca19a2c9cd
Josh E <45578375+uaon@users.noreply.github.com>
2022-07-14 09:02:04 -0500

    apicache rm
    rm apicache

--- server/server.js
+++ server/server.js
@@ -1,13 +1,8 @@
 const middleware = require("./routes");
 const express = require("express");
-const apicache = require("apicache");
 const app = express()
 const cors = require("cors");
 const port = 4000;
-
-// cache
-let cache = apicache.middleware
-app.use(cache('5 minutes'))
 
 app.use(cors({
   origin: '*',
