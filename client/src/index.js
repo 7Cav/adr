7bf377722c6913b2cb9a9c90c29efd17c9dc9ea7
Josh E <45578375+uaon@users.noreply.github.com>
2022-09-12 00:42:08 -0500

    use native script on clarity

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Card />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
