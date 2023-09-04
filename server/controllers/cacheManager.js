6f4b70e2828547d957ea7bd3a5a1df915cbada70
Ron <66834451+SyniRon@users.noreply.github.com>
2023-09-03 22:37:37 -0400

    Formatting and minor work (#37)
    * prettier
    
    * remove .env from git
    
    * add linter
    
    * add eslint and build check
    
    * fix css for logo centering
    
    * remember to install
    
    * linter fix
    
    * maybe fix for eslint
    
    * remove unused extends
    
    * remove eslint
    
    * Fix prettier yaml
    
    * pin prettier version
    
    * fix formatting

--- server/controllers/cacheManager.js
+++ server/controllers/cacheManager.js
@@ -6,62 +6,63 @@ let cachedReserveRoster;
 let cacheTime = {};
 
 const updateCombatRosterCache = async () => {
-    try {
-        const response = await axios('https://api.7cav.us/api/v1/roster/1', {
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + API_TOKEN
-            },
-        });
-        cachedCombatRoster = response.data;
-        cacheTime['combat'] = Date.now();
-    } catch (error) {
-        console.error("Failed to update combat cache:", error);
-    }
+  try {
+    const response = await axios("https://api.7cav.us/api/v1/roster/1", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + API_TOKEN,
+      },
+    });
+    cachedCombatRoster = response.data;
+    cacheTime["combat"] = Date.now();
+  } catch (error) {
+    console.error("Failed to update combat cache:", error);
+  }
 };
 
 const updateReserveRosterCache = async () => {
-    try {
-        const response = await axios('https://api.7cav.us/api/v1/roster/2', {
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + API_TOKEN
-            },
-        });
-        cachedReserveRoster = response.data;
-        cacheTime['reserve'] = Date.now();
-    } catch (error) {
-        console.error("Failed to update reserve cache:", error);
-    }
+  try {
+    const response = await axios("https://api.7cav.us/api/v1/roster/2", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + API_TOKEN,
+      },
+    });
+    cachedReserveRoster = response.data;
+    cacheTime["reserve"] = Date.now();
+  } catch (error) {
+    console.error("Failed to update reserve cache:", error);
+  }
 };
 
-const scheduleCacheUpdate = (updateFunction, key) => {
-    const now = new Date();
-    const delay = (60 - now.getMinutes()) * 60 * 1000 + (60 - now.getSeconds()) * 1000;
+const scheduleCacheUpdate = (updateFunction) => {
+  const now = new Date();
+  const delay =
+    (60 - now.getMinutes()) * 60 * 1000 + (60 - now.getSeconds()) * 1000;
 
-    setTimeout(() => {
-        updateFunction();
-        setInterval(updateFunction, 3660 * 1000); // Update every 3660 seconds (1 hour and 1 minute)
-    }, delay);
+  setTimeout(() => {
+    updateFunction();
+    setInterval(updateFunction, 3660 * 1000); // Update every 3660 seconds (1 hour and 1 minute)
+  }, delay);
 };
 
 // Initialize the cache and schedule the updates
 updateCombatRosterCache();
-scheduleCacheUpdate(updateCombatRosterCache, 'combat');
+scheduleCacheUpdate(updateCombatRosterCache);
 updateReserveRosterCache();
-scheduleCacheUpdate(updateReserveRosterCache, 'reserve');
+scheduleCacheUpdate(updateReserveRosterCache);
 
 const getCachedCombatRoster = () => {
-    return cachedCombatRoster;
+  return cachedCombatRoster;
 };
 
 const getCachedReserveRoster = () => {
-    return cachedReserveRoster;
+  return cachedReserveRoster;
 };
 
 module.exports = {
-    getCachedCombatRoster,
-    getCachedReserveRoster
+  getCachedCombatRoster,
+  getCachedReserveRoster,
 };
