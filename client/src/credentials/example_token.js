db94e20e85e7b305c4df1b9e9f61d70312fa79a0
SyniRon <66834451+SyniRon@users.noreply.github.com>
2023-09-03 21:44:29 -0400

    fix formatting

--- client/src/credentials/example_token.js
+++ client/src/credentials/example_token.js
@@ -1,6 +1,5 @@
-const CLIENT_TOKEN = 'XXXXXXXX'
+const CLIENT_TOKEN = "XXXXXXXX";
 module.exports = CLIENT_TOKEN;
-
 
 //rename this file as "token.js" when in use.
 // should be in `../client/src/credentials/token.js` path.
