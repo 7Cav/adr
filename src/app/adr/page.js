aca042d7c8c25a9089a1cd53019427ac1ab4fd0b
Vercin-G <97210222+Vercin-G@users.noreply.github.com>
2023-10-09 12:21:52 -0600

    api testing

--- src/app/adr/page.js
+++ src/app/adr/page.js
@@ -1,3 +1,33 @@
-export default function Home() {
-  return <h1>Hello there! You are on the Active Duty Roster Page!</h1>;
+const CLIENT_TOKEN = process.env.REACT_APP_CLIENT_TOKEN;
+const combatApiUrl = process.env.REACT_APP_COMBAT_API_URL;
+const reserveApiUrl = process.env.REACT_APP_RESERVE_API_URL;
+const cacheTimestampUrl = process.env.REACT_APP_CACHE_TIMESTAMP_URL;
+const millisecondsToMinutes = (milliseconds) => {
+  return Math.round(milliseconds / (1000 * 60));
+};
+
+async function getcombatRoster() {
+  const response = await fetch(combatApiUrl, {
+    headers: {
+      Authorization: CLIENT_TOKEN,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("HTTP Error! status: " + response.status);
+  }
+
+  const data = await response.json();
+
+  return data;
+}
+
+export default async function ActiveDutyRoster() {
+  const combatRoster = await getcombatRoster;
+  console.log(combatRoster);
+  return (
+    <div>
+      <h1>Working</h1>
+    </div>
+  );
 }
