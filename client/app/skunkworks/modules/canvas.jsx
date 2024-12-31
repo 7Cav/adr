6cfed178bdc2471716bf31f494d3721b0d5160f6
Vercin-G <97210222+Vercin-G@users.noreply.github.com>
2024-12-31 12:41:46 -0700

    medal placement and layering testing

--- client/app/skunkworks/modules/canvas.jsx
+++ client/app/skunkworks/modules/canvas.jsx
@@ -6,15 +6,7 @@ const Canvas = (props) => {
 
   const data = props.data;
 
-  console.log(data);
-
-  let rankOrder;
-  let rankGrade;
-
-  if (data[0].rank == "2LT") {
-    rankOrder = "Officer";
-    rankGrade = "O1";
-  }
+  console.log(data[1]);
 
   useEffect(() => {
     const uniformBase = new Image();
@@ -24,18 +16,45 @@ const Canvas = (props) => {
     uniformLapel.src = "/skunkworks/uniformBase/uniformRightLapel.png";
 
     const uniformEpaulette = new Image();
-    uniformEpaulette.src = `skunkworks/uniformOfficer/officerEpaulettes/${rankGrade}.png`;
+    uniformEpaulette.src = `skunkworks/uniformOfficer/officerEpaulettes/${data[0].rankGrade}.png`;
+
+    const ribbonSprites = new Image();
+    ribbonSprites.src =
+      "skunkworks/uniformRibbons/ribbons/ribbonSpriteSheetAlt.png";
 
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
+
     context.fillStyle = "#000000";
     context.fillRect(0, 0, context.canvas.width, context.canvas.height);
 
     uniformBase.onload = () => {
       context.drawImage(uniformBase, 0, 0);
+      for (let i = 1; i < 2; i++) {
+        placeRibbon(data[i], ribbonSprites);
+      }
       context.drawImage(uniformLapel, 0, 0);
       context.drawImage(uniformEpaulette, 0, 0);
     };
+
+    function placeRibbon(data, ribbonSprites) {
+      const ribbonWidth = 43;
+      const ribbonHeight = 14;
+
+      const ribbonSelection = data.awardPriority + 1;
+
+      return context.drawImage(
+        ribbonSprites,
+        0,
+        ribbonSelection * ribbonHeight,
+        ribbonWidth,
+        ribbonHeight,
+        524,
+        245,
+        ribbonWidth,
+        ribbonHeight
+      );
+    }
   }, []);
 
   return <canvas ref={canvasRef} {...props} width={837} height={1025} />;
