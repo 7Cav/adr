3e52690d1ef87aa8efbe1747fb2cf34b30f70448
SyniRon <66834451+SyniRon@users.noreply.github.com>
2025-01-04 00:19:50 -0500

    remove debug logging and make pretty

--- server/controllers/cacheManager.js
+++ server/controllers/cacheManager.js
@@ -26,10 +26,9 @@ const updateCombatRosterCache = async () => {
         Accept: "application/json",
         "Content-Type": "application/json",
         Authorization: "Bearer " + API_TOKEN,
-        'Accept-Encoding': 'gzip',
+        "Accept-Encoding": "gzip",
       },
     });
-    console.log("Request Headers:", response.config.headers);
     cachedCombatRoster = response.data;
     cacheTime["combat"] = Date.now();
     cacheStatus.combat = true;
@@ -46,10 +45,9 @@ const updateReserveRosterCache = async () => {
         Accept: "application/json",
         "Content-Type": "application/json",
         Authorization: "Bearer " + API_TOKEN,
-        'Accept-Encoding': 'gzip',
+        "Accept-Encoding": "gzip",
       },
     });
-    console.log("Request Headers:", response.config.headers);
     cachedReserveRoster = response.data;
     cacheTime["reserve"] = Date.now();
     cacheStatus.reserve = true;
