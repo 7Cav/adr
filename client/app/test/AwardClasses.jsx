d8207d4a181bec0d61492eb4f6cc4362a87400bf
Vercin-G <97210222+Vercin-G@users.noreply.github.com>
2025-03-15 14:57:40 -0600

    day by day

---
+++ client/app/test/AwardClasses.jsx
@@ -0,0 +1,57 @@
+export class Award {
+  awardTitle = null;
+  //awardDetail = null; May not be needed if exclusive to badges
+  awardPriority = null;
+
+  constructor(data) {
+    this.awardTitle = data.awardName;
+  }
+}
+
+export class Ribbon extends Award {
+  static totalRibbonCount = 0;
+  hasValorDevice = null;
+  maxAwardcount = null;
+  ribbonAttachmentType = null;
+  ribbonAttachmentCount = 0;
+
+  constructor(data, AwardRegistry) {
+    super(data);
+
+    const registryDetails = AwardRegistry.getAwardDetails(data.awardName);
+
+    if (!(registryDetails.awardAttachmentType === undefined)) {
+      this.ribbonAttachmentType = registryDetails.awardAttachmentType;
+    }
+
+    this.awardPriority = registryDetails.awardPriority;
+
+    Ribbon.totalRibbonCount++;
+  }
+}
+
+export class Medal extends Ribbon {
+  static totalMedalCount = 0;
+
+  constructor(data, AwardRegistry) {
+    super(data, AwardRegistry);
+    if (this.awardTitle.includes("with Valor Device")) {
+      this.overrideAwardTitle(data.awardName);
+    }
+    Medal.totalMedalCount++;
+  }
+
+  overrideAwardTitle(awardName) {
+    const baseAwardName = awardName.replace(" with Valor Device", "");
+    this.awardTitle = baseAwardName;
+    this.hasValorDevice = true;
+  }
+}
+
+export class Badge extends Award {
+  // Do things
+}
+
+export class unitCitation extends Ribbon {
+  // Do moar things
+}
