38f8f67c1b3749f16d155e41a29df5e8f4f438de
Vercin-G <97210222+Vercin-G@users.noreply.github.com>
2025-05-15 11:47:12 -0600

    Implement 10 min cache control for skunkworks

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/_next/static/chunks/app/skunkworks/:path*.js",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=600, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+};
