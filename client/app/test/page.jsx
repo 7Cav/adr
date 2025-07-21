5783c5ce55e49abdd58eff038419ac9890c38ce3
Vercin-G <97210222+Vercin-G@users.noreply.github.com>
2025-07-21 13:29:23 -0600

    import current ADR code to set up dev enviornment

--- client/app/test/page.jsx
+++ client/app/test/page.jsx
@@ -3,9 +3,10 @@ import GetCombatRoster from "../reusableModules/getCombatRoster";
 import GetReserveRoster from "../reusableModules/getReserveRoster";
 import GetApiTimestamp from "../reusableModules/getApiTimestamp";
 import GetRosterGroups from "../reusableModules/getGroups";
+import AdrListEntry from "./modules/AdrListEntry";
 import Logo from "../theme/adrLogo";
-//import "./page.css";
-//import "../globals.css";
+import "./page.css";
+import "../globals.css";
 
 export const metadata = {
   title: "Active Duty Roster",
@@ -25,5 +26,46 @@ export default async function ActiveDutyRoster() {
   console.log(milpacArray);
   console.log(rosterGroups);
 
-  return <h1>Hello there!</h1>;
+  return (
+    <div className="MasterContainer">
+      <div className="p-nav-primary">
+        <div className="p-nav-wrapper">
+          <nav className="p-nav">
+            <div className="p-nav-inner">
+              <div className="p-nav-scroller">
+                <div className="p-nav-logo">
+                  <Link href={"/"}>
+                    <Logo
+                      alt="ADR Logo"
+                      title="Return to CavApps"
+                      width="17em"
+                      height="3em"
+                    />
+                  </Link>
+                </div>
+                <div className="p-nav-info">
+                  {timestamp && timestamp[0]?.combat !== null && (
+                    <div className="cache-time">
+                      Database is {timestamp[0].combat} minutes old
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+      <div className="ListContainer">
+        {/* note: bBGroup = Billet Bank Group */}
+        <AdrListEntry bBGroup="regi" milpacArray={milpacArray} />
+        <AdrListEntry bBGroup="oneSeven" milpacArray={milpacArray} />
+        <AdrListEntry bBGroup="twoSeven" milpacArray={milpacArray} />
+        <AdrListEntry bBGroup="threeSeven" milpacArray={milpacArray} />
+        <AdrListEntry bBGroup="acd" milpacArray={milpacArray} />
+        <AdrListEntry bBGroup="secOps" milpacArray={milpacArray} />
+        <AdrListEntry bBGroup="roo" milpacArray={milpacArray} />
+        <AdrListEntry bBGroup="support" milpacArray={milpacArray} />
+      </div>
+    </div>
+  );
 }
