143f677855a190e2a1053cb9f9fdb8915e528a52
Vercin-G <97210222+Vercin-G@users.noreply.github.com>
2025-07-21 13:29:23 -0600

    import current ADR code to set up dev enviornment

---
+++ client/app/test/modules/AdrListEntry.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import MilpacParse from "./MilpacParse";
+import lists from "../../reusableModules/BilletBank";
+
+function AdrListEntry(props) {
+  let billetBankObject = lists.billetBankObject;
+  let selector = props.bBGroup;
+
+  return (
+    <div className="DepartmentContainer">
+      <div className="Title">{billetBankObject[selector].collapsibleTitle}</div>
+      <div className="ResponseContainer">
+        <MilpacParse
+          milpacArray={props.milpacArray}
+          headerTitles={billetBankObject[selector].positionTitles}
+          billetBankObject={billetBankObject[selector].positionIds}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default AdrListEntry;
